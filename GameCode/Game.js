416c62cc414496dd242d59fc35cdbc35c533be17
TamaraL122004 <tamara.lamas@ucuenca.edu.ec>
2025-01-19 18:01:15 -0500

    Creación del laberinto

--- GameCode/Game.js
+++ GameCode/Game.js
@@ -4,11 +4,44 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
 // Configuración inicial
-canvas.width = 900; // Ajusta según el tamaño del laberinto
+canvas.width = 1050; // Ajusta según el tamaño del laberinto
 canvas.height = 600;
 
-const tileMap = new TileMap(60); // Tamaño de cada celda del laberinto
+let tileMap;
+const tileSize = 75; // Tamaño de cada celda en píxeles
+let wallImage, floorImage; // Variables para las imágenes
 let gameStarted = false;
+
+// Preload de imágenes
+function preloadImages() {
+  wallImage = new Image();
+  wallImage.src = "./src/wall.png";
+
+  floorImage = new Image();
+  floorImage.src = "./src/Piso.png";
+
+  wallImage.onload = () => console.log("Imagen de muro cargada");
+  floorImage.onload = () => console.log("Imagen de piso cargada");
+}
+
+// Configurar el juego
+function setup() {
+  tileMap = new TileMap(tileSize, wallImage, floorImage);
+}
+
+// Dibujar el juego
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpia el canvas
+  tileMap.draw(ctx); // Dibuja el laberinto
+}
+
+// Ciclo del juego
+function gameLoop() {
+  draw();
+  if (gameStarted) {
+    requestAnimationFrame(gameLoop);
+  }
+}
 
 // Iniciar el juego
 function startGame() {
@@ -18,13 +51,7 @@ function startGame() {
   gameLoop();
 }
 
-// Ciclo del juego
-function gameLoop() {
-  tileMap.draw(ctx); // Dibujar el laberinto
-  if (gameStarted) {
-    requestAnimationFrame(gameLoop);
-  }
-}
-
-// Evento para iniciar el juego
+// Prepara el juego y añade eventos
+preloadImages();
+setup();
 document.getElementById("start-button").addEventListener("click", startGame);
--- GameCode/TileMap.js
+++ GameCode/TileMap.js
@@ -1,34 +1,36 @@
 export default class TileMap {
-  constructor(tileSize) {
-    this.tileSize = tileSize;
-
-    // Matriz que define el laberinto (1 = pared, 0 = camino)
-    this.map = [
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-      [1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1],
-      [1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-      [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-      [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1],
-      [1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1],
-      [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ];
+  constructor(tileSize, wallImage, floorImage) {
+    this.tileSize = tileSize; // Tamaño de cada celda
+    this.wallImage = wallImage; // Imagen de la pared
+    this.floorImage = floorImage; // Imagen del suelo
   }
 
+  // Matriz que define el laberinto (1 = pared, 0 = camino)
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  // Dibujar el laberinto
   draw(ctx) {
     for (let row = 0; row < this.map.length; row++) {
       for (let col = 0; col < this.map[row].length; col++) {
-        const tile = this.map[row][col];
+        let tile = this.map[row][col];
+        let x = col * this.tileSize;
+        let y = row * this.tileSize;
+
+        // Dibuja el suelo en todas las celdas
+        ctx.drawImage(this.floorImage, x, y, this.tileSize, this.tileSize);
+
+        // Dibuja los muros donde corresponda
         if (tile === 1) {
-          ctx.fillStyle = "grey"; // Color de las paredes
-          ctx.fillRect(
-            col * this.tileSize,
-            row * this.tileSize,
-            this.tileSize,
-            this.tileSize
-          );
+          ctx.drawImage(this.wallImage, x, y, this.tileSize, this.tileSize);
         }
       }
     }
